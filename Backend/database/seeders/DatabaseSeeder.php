98f18939c1000ad952645f1636e089758e42cd53
LATIFA-ouamou <latifaouamou4444@gmail.com>
2025-12-14 01:30:33 +0100

    models

--- Backend/database/seeders/DatabaseSeeder.php
+++ Backend/database/seeders/DatabaseSeeder.php
@@ -18,5 +18,12 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+          $this->call([
+            AdminSeeder::class,
+            UserSeeder::class,
+            BookSeeder::class,
+            BorrowSeeder::class,
+        ]);
     }
 }
--- Backend/database/seeders/UserSeeder.php
+++ Backend/database/seeders/UserSeeder.php
@@ -2,17 +2,17 @@
 
 namespace Database\Seeders;
 
-use App\Models\Book;
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class BookSeeder extends Seeder
+class UserSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        Book::factory(20)->create();
+       User::factory(10)->create();
     }
 }
